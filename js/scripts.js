48f719db40f1f9f8a8576a23d0856570a5344441
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-09-24 18:08:20 +0200

    add javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ var add= function(number1, number2){
     return number1 +number2;
     };
     var number1= parseInt(prompt("Enter number:"))
-    var number2= parseInt(prompt("Enter another number:"))
-    alert(add(number1, number2))
+    var number2= parseInt(prompt("Enter another number:"));
+    var result= add(number1,number2);
+    alert(result)
